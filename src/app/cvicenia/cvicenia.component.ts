3ad27be18856ade4c707b66540b1a6692d4d2e95
karolsinko <karol.sinko30@gmail.com>
2022-12-10 19:39:14 +0100

    Bakalárska práca 1.2 (Implementácia online compileru v2)

--- src/app/cvicenia/cvicenia.component.ts
+++ src/app/cvicenia/cvicenia.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+declare function showTab(): any;
+declare function nextPrev(): any;
+declare function validateForm(): any;
+declare function fixStepIndicator(): any;
 
 @Component({
   selector: 'app-cvicenia',
@@ -8,24 +12,28 @@ import {Router} from "@angular/router";
 })
 export class CviceniaComponent implements OnInit {
 
-  myScriptElement: HTMLScriptElement;
-  //myScriptElement1: HTMLIFrameElement;
+ /* myScriptElement: HTMLScriptElement;*/
+  myScriptElement1: HTMLIFrameElement;
 
   constructor(private router: Router) {
-  this.myScriptElement = document.createElement("script");
+  /*this.myScriptElement = document.createElement("script");
   this.myScriptElement.src = "https://www.jdoodle.com/assets/jdoodle-pym.min.js";
-  document.body.appendChild(this.myScriptElement)
-  /*this.myScriptElement1 = document.createElement("iframe");
-  this.myScriptElement1.src = "https://repl.it/@zyvxn/DeveloperComEmbeddedRepl?lite=true";
-  this.myScriptElement1.width = "2000px";
-  this.myScriptElement1.height = "800px";
-  document.body.appendChild(this.myScriptElement1);*/
+  document.body.appendChild(this.myScriptElement);*/
+  this.myScriptElement1 = document.createElement("iframe");
+  this.myScriptElement1.src = "https://paiza.io/projects/e/y4_khhZvaf4VAfBGlPCuIQ?theme=twilight";
+  this.myScriptElement1.width = "1000px";
+  this.myScriptElement1.height = "1000px";
+  document.body.appendChild(this.myScriptElement1);
 }
 
   chodSpat(): void {
     this.router.navigate(['']);
   }
   ngOnInit(): void{
+    showTab();
+    nextPrev();
+    validateForm();
+    fixStepIndicator();
   }
 
 }
