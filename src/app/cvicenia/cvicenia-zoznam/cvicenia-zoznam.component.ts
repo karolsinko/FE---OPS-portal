d9d41bf071bd09fcdd28e2cad9c2bc4e769c3f6c
karolsinko <karol.sinko30@gmail.com>
2023-01-13 16:03:21 +0100

    Bakalárska práca 1.6 (Fungujúci stepper pre cvicenia)

--- src/app/cvicenia/cvicenia-zoznam/cvicenia-zoznam.component.ts
+++ src/app/cvicenia/cvicenia-zoznam/cvicenia-zoznam.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {zoznamUloh} from "../../models/cvicenie.model";
 
 @Component({
@@ -7,12 +7,33 @@ import {zoznamUloh} from "../../models/cvicenie.model";
   styleUrls: ['./cvicenia-zoznam.component.css']
 })
 export class CviceniaZoznamComponent implements OnInit {
+  currentStepIndex = 0;
 
-  constructor() { }
+  constructor() {
 
-  ngOnInit(): void {
   }
+
   @Input()
   ulohy: zoznamUloh[] = [];
 
+  step = [
+    { title: 'Úloha 1', content: 'Step 1 content goes here' },
+    { title: 'Úloha 2', content: 'Step 2 content goes here' },
+    { title: 'Úloha 3', content: 'Step 3 content goes here' }
+  ];
+
+  previousStep() {
+    if (this.currentStepIndex > 0) {
+      this.currentStepIndex--;
+    }
+  }
+
+  nextStep() {
+    if (this.currentStepIndex < this.step.length - 1) {
+      this.currentStepIndex++;
+    }
+  }
+
+  ngOnInit(): void {
+  }
 }
