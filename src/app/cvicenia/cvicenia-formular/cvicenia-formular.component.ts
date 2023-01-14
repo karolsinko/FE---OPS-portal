40ad7b105d86514efc8ecdbf0c6e2dc63848e36a
karolsinko <karol.sinko30@gmail.com>
2023-01-14 15:00:26 +0100

    Bakalárska práca 1.7 (Úprava kompozície úloh, pridanie linux terminalu, pridanie linux pridavanie a odstranenie terminalu, uprava CSS layoutu)

--- src/app/cvicenia/cvicenia-formular/cvicenia-formular.component.ts
+++ src/app/cvicenia/cvicenia-formular/cvicenia-formular.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, Input, OnInit, Output} from '@angular/core';
 import {Router} from "@angular/router";
 
 @Component({
@@ -11,17 +11,23 @@ export class CviceniaFormularComponent implements OnInit {
   /* myScriptElement: HTMLScriptElement;*/
   compilerC: HTMLIFrameElement;
   compilerBash: HTMLIFrameElement;
+  compilerLinux: HTMLIFrameElement;
 
   constructor(private router: Router) {
     this.compilerC = document.createElement("iframe");
     this.compilerC.src = "https://paiza.io/projects/e/y4_khhZvaf4VAfBGlPCuIQ?theme=twilight";
-    this.compilerC.height = "1000px";
-    this.compilerC.width = "1000px";
+    this.compilerC.height = "500";
+    this.compilerC.width = "auto";
 
     this.compilerBash = document.createElement("iframe");
     this.compilerBash.src = "https://paiza.io/projects/e/lGDP8OLLGPWOYsHkXjjwmg?theme=twilight";
-    this.compilerBash.height = "1000px";
-    this.compilerBash.width = "1000px";
+    this.compilerBash.height = "500";
+    this.compilerBash.width = "auto";
+
+    this.compilerLinux = document.createElement("iframe");
+    this.compilerLinux.src = "https://bellard.org/jslinux/vm.html?cpu=riscv64&url=buildroot-riscv64.cfg&mem=256";
+    this.compilerLinux.height = "700";
+    this.compilerLinux.width = "auto";
   }
 
   chodSpat(): void {
@@ -44,6 +50,13 @@ export class CviceniaFormularComponent implements OnInit {
     document.getElementById("here")?.removeChild(this.compilerBash);
   }
 
+  pridajCompilerLinux(): void {
+    document.getElementById("here")?.appendChild(this.compilerLinux);
+  }
+
+  odstranCompilerLinux(): void {
+    document.getElementById("here")?.removeChild(this.compilerLinux);
+  }
   ngOnInit(): void{
 
   }
--- src/app/cvicenia/cvicenia-zoznam/cvicenia-zoznam.component.ts
+++ src/app/cvicenia/cvicenia-zoznam/cvicenia-zoznam.component.ts
@@ -17,7 +17,7 @@ export class CviceniaZoznamComponent implements OnInit {
   ulohy: zoznamUloh[] = [];
 
   step = [
-    { title: 'Úloha 1', content: 'Step 1 content goes here' },
+    { title: '1', content: 'Vašou úlohou je vytvoriť pre automobilový bazár organizáciu áut dostupných na predaj. Pozor ale! Každá značka musí byť osobitne a zároveň každý model v nej. Bazár aktuálne ponúka jeden model od značky BMW s názvom M3 a dva modely od značky AUDI s názvami A3 a A4.' },
     { title: 'Úloha 2', content: 'Step 2 content goes here' },
     { title: 'Úloha 3', content: 'Step 3 content goes here' }
   ];
