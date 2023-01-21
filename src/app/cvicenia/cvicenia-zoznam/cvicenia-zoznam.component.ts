a9a0246fa2f105ca32040e9d1e67153ef70d43b5
karolsinko <karol.sinko30@gmail.com>
2023-01-21 19:02:47 +0100

    Bakalárska práca 1.10 (zobrazovanie vysledkov, pridanie funkcionality novej)

--- src/app/cvicenia/cvicenia-zoznam/cvicenia-zoznam.component.ts
+++ src/app/cvicenia/cvicenia-zoznam/cvicenia-zoznam.component.ts
@@ -16,11 +16,18 @@ export class CviceniaZoznamComponent implements OnInit {
   @Input()
   ulohy: zoznamUloh[] = [];
 
-  step = [
-    { input: 'Pokúste sa úlohu splniť čo najoptimalnejšou formou (optimálne riešenie je možné vytvoriť z jedného riadka")',content: 'Vašou úlohou je vytvoriť pre automobilový bazár organizáciu áut dostupných na predaj. Pozor ale! Každá značka musí byť osobitne a zároveň každý model v nej. Bazár aktuálne ponúka jeden model od značky BMW s názvom M3 a dva modely od značky AUDI s názvami A3 a A4.' },
-    { title: 'Úloha 2', content: 'Step 2 content goes here' },
+  uloha = [
+    { input: 'Pokúste sa úlohu splniť čo najoptimalnejšou formou (optimálne riešenie je možné vytvoriť z jedného riadka")',
+      content: 'Vašou úlohou je vytvoriť pre automobilový bazár organizáciu áut dostupných na predaj. Pozor ale! Každá značka musí byť osobitne a zároveň každý model v nej. Bazár aktuálne ponúka jeden model od značky BMW s názvom M3 a dva modely od značky AUDI s názvami A3 a A4.',
+      vysledok: 'mkdir -p BMW/M3 AUDI/A3 AUDI/A4'
+    },
+    { title: 'Úloha 2',
+      content: '',
+      vysledok: 'nic2'
+    },
     { title: 'Úloha 3', content: 'Step 3 content goes here' }
   ];
+
 
   previousStep() {
     if (this.currentStepIndex > 0) {
@@ -30,7 +37,7 @@ export class CviceniaZoznamComponent implements OnInit {
   }
 
   nextStep() {
-    if (this.currentStepIndex < this.step.length - 1) {
+    if (this.currentStepIndex < this.uloha.length - 1) {
       this.currentStepIndex++;
       this.updateStepIndicator();
     }
@@ -46,6 +53,13 @@ export class CviceniaZoznamComponent implements OnInit {
       }
     });
   }
+
+  showElement = false
+
+  showSolution(){
+    this.showElement = !this.showElement;
+  }
+
   ngOnInit(): void {
   }
 }
