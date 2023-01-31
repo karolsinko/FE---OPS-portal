62e37c772e23adac64dda688f3b5410910a1e599
karolsinko <karol.sinko30@gmail.com>
2023-01-31 15:07:21 +0100

    Bakalárska práca 1.37 (Quiz 1.4)

--- src/app/models/quiz.model.ts
+++ src/app/models/quiz.model.ts
@@ -6,15 +6,17 @@ export class quiz{
   option1: string;
   option2: string;
   option3: string;
+  option4: string;
 
 
-  constructor(id: number, solution: string, question: string, option1: string, option2: string, option3: string) {
+  constructor(id: number, solution: string, question: string, option1: string, option2: string, option3: string,   option4: string) {
     this.id = id;
     this.solution = solution;
     this.question = question;
     this.option1 = option1;
     this.option2 = option2;
     this.option3 = option3;
+    this.option4 = option4;
   }
 }
 
@@ -26,6 +28,7 @@ export class zoznamQuizov{
   option1: string;
   option2: string;
   option3: string;
+  option4: string;
 
   constructor(quizov: zoznamQuizov) {
     this.id = quizov.id;
@@ -34,5 +37,6 @@ export class zoznamQuizov{
     this.option1 = quizov.option1;
     this.option2 = quizov.option2;
     this.option3 = quizov.option3;
+    this.option4 = quizov.option4;
   }
 }
--- src/app/quiz/quiz-zoznam/quiz-zoznam.component.ts
+++ src/app/quiz/quiz-zoznam/quiz-zoznam.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {zoznamQuizov} from "../../models/quiz.model";
-import {HttpClient} from "@angular/common/http";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-quiz-zoznam',
@@ -13,11 +13,38 @@ export class QuizZoznamComponent implements OnInit {
   @Input()
   quiz: zoznamQuizov[] = [];
 
+  @Input()
+  set quizov(data: zoznamQuizov | undefined){
+    if (data) {
+      this.fillForm(data);
+    }
+  }
+
   currentStepIndex = 0;
   score = 0
 
-  constructor(private http: HttpClient) {}
+  constructor() {
+    this.form = new FormGroup({
+      id: new FormControl(null),
+      option1: new FormControl(null),
+      option2: new FormControl(null),
+      option3: new FormControl(null),
+      option4: new FormControl(null),
+      question: new FormControl(null),
+      solution: new FormControl(null)
+    });
+  }
+  form: FormGroup;
 
+  private fillForm(quizov: zoznamQuizov): void {
+    this.form.controls['id'].setValue(quizov.id);
+    this.form.controls['option1'].setValue(quizov.option1);
+    this.form.controls['option2'].setValue(quizov.option2);
+    this.form.controls['option3'].setValue(quizov.option3);
+    this.form.controls['option4'].setValue(quizov.option4);
+    this.form.controls['question'].setValue(quizov.question);
+    this.form.controls['solution'].setValue(quizov.solution);
+  }
   ngOnInit() {
   }
 
@@ -26,16 +53,17 @@ export class QuizZoznamComponent implements OnInit {
       const answer = this.quiz[i].option2;
       const option2 = this.quiz[i].solution;
         if (answer === option2) {
-        } else {
-          alert(`Question ${i + 1} is incorrect. Correct answer is "${option2}"`);
-          return;
         }
+            else {
+              alert(`Question ${i + 1} is incorrect. Correct answer is "${option2}"`);
+              this.score = 0;
+            }
     }
     if (this.currentStepIndex < this.quiz.length-1) {
       this.currentStepIndex++;
-
     }
     this.score++;
     console.log(`Your score is: ${this.score}`);
+    return 0;
   }
 }
