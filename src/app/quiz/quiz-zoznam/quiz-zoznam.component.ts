881f4a026c956fa804e0e5eed08b85f82ed7bf34
karolsinko <karol.sinko30@gmail.com>
2023-01-31 13:39:06 +0100

    Bakalárska práca 1.34 (Quiz 1.1)

--- src/app/quiz/quiz-zoznam/quiz-zoznam.component.ts
+++ src/app/quiz/quiz-zoznam/quiz-zoznam.component.ts
@@ -13,11 +13,23 @@ export class QuizZoznamComponent implements OnInit {
   @Input()
   quiz: zoznamQuizov[] = [];
 
-  answers = {};
-
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
   }
 
+  submitAnswers() {
+    let score = 0;
+    for (let i = 0; i < this.quiz.length; i++) {
+      const answer = this.quiz[i].option1;
+      const correctAnswer = this.quiz[i].option2;
+      if (answer === correctAnswer) {
+        score++;
+      } else {
+        alert(`Question ${i + 1} is incorrect. Correct answer is "${correctAnswer}"`);
+        return;
+      }
+    }
+    console.log(`Your score is: ${score}`);
+  }
 }
